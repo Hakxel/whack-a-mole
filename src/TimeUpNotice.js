957ce371af4e8e1d5cc9a5fcad1f63dfcd933678
Hakxel <asiel_montes@yahoo.com>
2019-04-05 14:08:20 -0400

    Improve timeup notice

--- src/TimeUpNotice.js
+++ src/TimeUpNotice.js
@@ -5,7 +5,7 @@ const TimeUpNotice = () => {
   return(
     <div className="time-up">
       <div className="timeup-content">
-        <h3>Time's up buddy!</h3>
+        <h3>Time's up, buddy!</h3>
         <h2>Let's see how many points you got...</h2>
       </div>
     </div>
